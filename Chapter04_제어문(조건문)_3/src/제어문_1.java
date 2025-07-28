d420bbe59d0de97f4645a1f5939762068ee63d5d
sist <sist@sist37>
2025-07-28 16:48:57 +0900

    1111

---
+++ Chapter04_제어문(조건문)_3/src/제어문_1.java
@@ -0,0 +1,45 @@
+/*
+ * 	단일 조건문
+ * 	  형식)
+ * 			if(조건문) : 조건문(true/false)인 연산자 사용
+ * 					   => 부정연산자(!), 비교연산자, 논리연산자만 사용 가능
+ * 			{
+ * 				조건이 true일때 수행하는 문장
+ * 				*** false면 if를 건너뛴다
+ * 			}
+ * 	선택조건문
+ * 	  형식) => true일때 false일때 나눠서 작업
+ * 		   if(조건문)
+ * 		   {
+ * 				조건 true일때 수행
+ * 		   }
+ * 		   else
+ * 		   {
+ * 			 	조건 false일때 수행
+ * 		   }
+ * 		*** 단일조건문 독립적이다 => if마다 따로 수행
+ * 		다중조건문 : 한개의 문장으로 만들어져 있다
+ * 				  => 조건에 맞는 문장 1개만 수행
+ * 				  => 선택문 (switch)
+ * 		if(조건문)
+ * 		{
+ * 			조건 true => 문장 수행 => 종료
+ * 			조건 false
+ * 				|
+ * 		}
+ * 		else if(조건문)
+ * 		{
+ * 			조건 true => 문장 수행 => 종료
+ * 				조건 false
+ * 					|
+ * 		{
+ * 			 
+ */		
+public class 제어문_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
