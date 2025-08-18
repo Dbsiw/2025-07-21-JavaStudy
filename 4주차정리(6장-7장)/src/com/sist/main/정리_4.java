34a6a2f99c72169ac998fbd9ab979273c3abfbb4
sist <sist@sist37>
2025-08-18 09:31:00 +0900

    1111

---
+++ 4주차정리(6장-7장)/src/com/sist/main/정리_4.java
@@ -0,0 +1,24 @@
+package com.sist.main;
+import java.util.*;
+class GenieMusic{
+	public void dataIn()
+	{
+		System.out.println("지니 뮤직 크롤링");
+	}
+	public void list()
+	{
+		System.out.println("목록 출력");
+	}
+	public void titlefiled()
+	{
+		System.out.println("지니 뮤직 크롤링");
+	}
+	
+}
+public class 정리_4 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
