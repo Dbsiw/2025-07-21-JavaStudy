91f578456f01b22f17d2e6c23abe8a48529e7f01
sist <sist@sist37>
2025-08-21 09:14:22 +0900

    1111

---
+++ Chapter12_라이브러리(java,lang)/src/com/sist/lang/라이브러리_5.java
@@ -0,0 +1,43 @@
+package com.sist.lang;
+/*
+ * java.lang
+ * 	1. object
+ * 	2. String
+ * 	3. StringBuffer
+ * 	4. System
+ * 	5. Math
+ * java.util
+ * 	***1. Collection
+ * 		= List
+ * 		= Set
+ * 		= Map
+ *  ***2. StringTokenizer
+ *  3. Scanner
+ *  **4. Arrays
+ *  ----------------------- 많이 사용되는 라이브러리
+ *  java.text
+ *   1. MessageFormat : SQL문장
+ *   -------------------------
+ *   2. SimpleDateFormat : 날짜
+ *   3. DecimalFormat 
+ *   ------------------------- 오라클
+ *  java.io
+ *  java.net
+ *  java.sql
+ *  --------------- 외부라이브러리 Jsoup / simple-json
+ *  				mvnrepository.com
+ *  --------------- J2SE => J2EE(웹)
+ *  						-------
+ *  단점 : 패키지가 변경 (웹)
+ *  	  tomcat => 9 : javax 10~ : jakarta
+ *  				--------- -------------
+ *	
+ */
+public class 라이브러리_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
