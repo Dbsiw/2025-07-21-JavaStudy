e6b4a4d836775853a17e68ee1ee31c3c998faf4e
sist <sist@sist37>
2025-08-27 16:47:14 +0900

    1111

---
+++ Chapter09_14_19_네트워크/src/com/sist/io/입출력_6.java
@@ -0,0 +1,37 @@
+package com.sist.io;
+// FileReader / FileWriter
+import java.util.*;
+import java.io.*;
+public class 입출력_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 파일 저장
+		Scanner scan=new Scanner(System.in);
+		System.out.println("이름:");
+		String name=scan.next();
+		System.out.println("성별(남자,여자):");
+		String sex=scan.next();
+		System.out.println("나이:");
+		int age=scan.nextInt();
+		
+		String msg=name+"|"+sex+"|"+age+"\r\n";
+		
+		try
+		{
+			File file=new File("c:\\upload\\sawon.txt");
+			if(!file.exists())
+			{
+				file.createNewFile();
+			}
+			
+			// 데이터를 출력
+			FileWriter fw=new FileWriter(file);
+			// =====> w(create) : 덮어쓴다  , a => 추가 
+			fw.write(msg);
+			fw.close();
+			System.out.println("데이터 저장 완료");
+		}catch(Exception ex) {}
+	}
+
+}
